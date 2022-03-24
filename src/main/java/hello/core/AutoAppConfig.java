4ffacf6afd68d1a94fdfcdbc4a7506a1529b83a9
KimBoSung <ghalsru@naver.com>
2022-03-24 21:03:15 +0900

    @Component와 @Autowired @Bean으로 직접 등록하지 않아도 @Component로 등록하고 @Autowired로 의존관계 자동 주입

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+) // 기존에 만든 AppConfig등 Configuration 어노테이션들을 예외시키는 코드(테스트를 위해)
+public class AutoAppConfig {
+
+}
