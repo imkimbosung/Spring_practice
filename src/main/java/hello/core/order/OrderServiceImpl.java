ab9e466dca8757ba2ae2acac3da3245c557d52f0
KimBoSung <ghalsru@naver.com>
2022-03-28 22:56:34 +0900

    2개 이상의 bean이 조회될때 @Qualifier를 이용하여 매칭

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,24 +5,25 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @RequiredArgsConstructor 는 final로 선언된 변수들을 가지고 생성자를 자동생성해줌.. 그래서 아래의 생성자 주석처리
-
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        /*
-//            생성자가 하나면 자동 @Autowired 적용된다. (@Component , @Bean 일때)
-//         */
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+//    @RequiredArgsConstructor 는 final로 선언된 변수들을 가지고 생성자를 자동생성해줌.. 그래서 아래의 생성자 주석처성
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        /*
+            생성자가 하나면 자동 @Autowired 적용된다. (@Component , @Bean 일때)
+         */
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
