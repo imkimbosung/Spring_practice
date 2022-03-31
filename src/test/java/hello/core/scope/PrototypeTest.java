3e06714fd4efb96ff234827ba8c37633aa5baae3
KimBoSung <ghalsru@naver.com>
2022-03-31 23:25:35 +0900

    싱글톤빈과 프로토타입빈의 차이점 싱글톤은 스프링 컨테이너가 종료시점까지 관리하기때문에 destroy를 실행하지만, 프로토타입빈은 초기화까지만 관리하기 때문에 직접 종료를 시켜야 한다.

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,43 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        //프로토타입 빈이기 때문에 destroy를 직접 시켜야 한다.(스프링 컨테이너가 초기화까지만 관리함)
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
