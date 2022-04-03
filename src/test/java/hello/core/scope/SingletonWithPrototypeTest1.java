8f76c8439382e297aa18be11a875014160952080
KimBoSung <ghalsru@naver.com>
2022-04-03 15:36:12 +0900

    java에서 제공하는 provider를 이용 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,17 +45,16 @@ public class SingletonWithPrototypeTest1 {
     @Scope("singleton")
     static class ClientBean{
 //        private final PrototypeBean prototypeBean; //생성시점에 주입되어있음
-//
-        @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
-
 //        @Autowired
 //        public ClientBean(PrototypeBean prototypeBean) {
 //            this.prototypeBean = prototypeBean;
 //        }
 
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
         public int logic(){
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
